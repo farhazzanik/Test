20673f034c68a5dcd783eca8e65428e4e59e572b
Farhazz anik <mahfuzkhan2125@gmail.com>
2020-12-07 16:51:21 +0000

    database configuration and connection

---
+++ dbConfig.php
@@ -0,0 +1,17 @@
+<?php
+
+	//database configuration
+	$dbHost = "localhost";
+	$dbUsername = "root";
+	$dbPassword = "";
+	$dbName = "movingworlds";
+
+	//create dabase connection
+	try {
+		$db = new PDO("mysql:host=$dbHost;dbname=$dbName",$dbUsername,$dbPassword)
+
+	} catch (PDOException $e) {
+		echo "Connection failed : ". $e->getMessage();
+	}
+
+?>
