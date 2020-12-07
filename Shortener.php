2add715cf794f0bfebea1c558c445c60c7cb1cf9
Farhazz anik <mahfuzkhan2125@gmail.com>
2020-12-07 20:34:13 +0000

    making function to check url exists in DB or Not

--- Shortener.php
+++ Shortener.php
@@ -31,7 +31,12 @@
 					throw new Exception("URL doesn't apear to exist...")
 				}
 			}
-		
+			$shortcode = $this->urlExistsInDb($url);
+			if($shortcode == false){
+				$shortcode = $this->createShortcode($url);
+			}
+
+			return $shortcode;
 		}
 
 		protected function validateUrlFormate($url){
@@ -40,14 +45,30 @@
 		}
 
 		protected function verifyUrlExists($url){
-			$ch =  crul_init($url);
+			$ch =  crul_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_NOBODY, true);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_exec($ch);
 			$response = curl_getinfo($ch,CURLINFO_HTTP_CODE);
-			curl_close($ch);
+			curl_close($ch)
+
+			return (!empty($response) && $response != 404);
 		}
+
+
+		protected function urlExistsInDb($url){
+			$query = "SELECT * FROM ".self::$table." WHERE long_url= :long_url limit 1";
+			$stmt = $this->pdo->prepare($query);
+			$param = array(
+				"long_url" => $url;
+			)
+			$stmt->execute($param);
+			$result = $stmt->fetch();
+			return (empty($result)) ? false : $result['short_code'];
+		}
+
+
 	}
 
 ?>
